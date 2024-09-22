ea89cdbd965924eb1f968139d2af272e034c50b0
mohanadft <mohanadfteha@gmail.com>
2024-09-22 03:01:27 +0300

    Parse Args and validate them

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+use regex::Regex;
+use std::fs;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Json File to parse
+    #[arg(value_parser = validate_json_file)]
+    file_name: String,
+}
+
+fn validate_json_file(file_name: &str) -> Result<String, String> {
+    let re = Regex::new(r"\.json$").unwrap();
+
+    if re.is_match(&file_name) {
+        Ok(file_name.to_string())
+    } else {
+        Err(String::from("The file must have a .json extension"))
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let Args { file_name } = Args::parse();
+
+    let content = fs::read_to_string(&file_name).unwrap();
+
+    println!("{content}");
 }
